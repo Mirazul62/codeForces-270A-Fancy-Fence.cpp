868629d9bb3f017bf6ab5efa732ef07921955a5e
Mirazul Islam <mirazkuetcse62@gmail.com>
2018-06-15 16:07:03 +0600

    270A	Fancy Fence solve in c++

---
+++ Fancy Fence.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n,i,a;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+      cin>>a;
+      if(360 %(a-180)==0)
+        cout<<"YES"<<endl;
+      else
+        cout<<"NO"<<endl;
+    }
+
+}
